db4741eccc6659fa70dc74825cf97a58bca50a3b
radlfabs <rosenthal.fabian@gmail.com>
2023-10-27 11:11:18 +0200

    add doc string

--- flexcv/run.py
+++ flexcv/run.py
@@ -2,6 +2,9 @@ from neptune.metadata_containers.run import Run as NeptuneRun
 
 
 class Run(NeptuneRun):
+    """Dummy Run class inheriting from Neptune Run.
+    It can be used to pass to all functions and methods that expect a Neptune Run Object.
+    It will not do anything."""
     def __init__(self, *args, **kwargs):
         self.run_id = "dummy"
 
