511068aa3feecfe52f4f2244a0f15e0191bde695
radlfabs <rosenthal.fabian@gmail.com>
2023-10-27 11:07:33 +0200

    update interface

--- flexcv/__init__.py
+++ flexcv/__init__.py
@@ -1,3 +1,4 @@
-from .interface import CrossValidation
+from .interface_functional import CrossValidation
 from .model_mapping import ModelMappingDict
 from .model_mapping import ModelConfigDict
+from .cv_split import CrossValMethod
