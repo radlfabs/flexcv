8c837ecc36de9ea577f7b39b244161ba93b5281c
radlfabs <rosenthal.fabian@gmail.com>
2023-11-08 17:34:13 +0100

    fix bug in repeated cv - key error when indexing for aggregation - type error of run_id when dummy run is passed - fixed instantiating inner CrossValidation class and setting of shared arguments

--- flexcv/repeated.py
+++ flexcv/repeated.py
@@ -18,12 +18,12 @@ def init_repeated_runs(n_repeats, neptune_credentials) -> tuple:
     The children runs are initialized with the same credentials as the parent run.
 
     Args:
-      n_repeats (int): Number of repeated runs.
-      use_neptune (bool): Whether to use neptune or not.
-      neptune_credentials (dict): Credentials for neptune.
+        n_repeats (int): Number of repeated runs.
+        use_neptune (bool): Whether to use neptune or not.
+        neptune_credentials (dict): Credentials for neptune.
 
     Returns:
-      (tuple[Run, list[Run]]): parent_run (Run), children_runs (list of n_repeats runs)
+        (tuple[Run, list[Run]]): parent_run (Run), children_runs (list of n_repeats runs)
 
     """
     parent_run = neptune.init_run(**neptune_credentials)
@@ -37,10 +37,10 @@ def try_mean(x):
     If x contains "NaN" and other values, -999 is returned.
 
     Args:
-      x: (array-like): Input array.
+        x: (array-like): Input array.
 
     Returns:
-      (float): Mean of the input array.
+        (float): Mean of the input array.
 
     """
     try:
@@ -59,15 +59,15 @@ def aggregate_(repeated_runs) -> pd.DataFrame:
     Therefore, the nested dict structure of the results is flattened.
     First, the model results are averaged over folds of individual runs.
     Second, the repeated (individual) runs are averaged.
-    
+
     Args:
-      repeated_runs: list: List of results of repeated runs.
+        repeated_runs: list: List of results of repeated runs.
 
     Returns:
-      (pd.DataFrame): Summary statistics of repeated runs.
-      
+        (pd.DataFrame): Summary statistics of repeated runs.
+
     Note:
-    
+
         The summary statistics are returned as a DataFrame with the following structure:
         - index: [aggregate]_[metric_name]
         - columns: [model_name]
@@ -77,14 +77,19 @@ def aggregate_(repeated_runs) -> pd.DataFrame:
 
     model_keys = list(repeated_runs[0].keys())
 
-    result_keys = repeated_runs[0][model_keys[0]]["metrics"].keys()
+    # we want the metrics keys 
+    # index [0] gives us first run
+    # index [model_keys[0]] -> gives us the dict for the first model
+    # index ["fold_by_metrics"] -> gives us a dict[metrics: list]
+    # .keys() -> gives us the keys of the metrics dict
+    result_keys = repeated_runs[0][model_keys[0]]["folds_by_metrics"].keys()
     results = []
     for result_key in result_keys:
         tmp_df = pd.DataFrame(
             [
                 pd.Series(
                     [
-                        try_mean(run[model_key]["metrics"][result_key])
+                        try_mean(run[model_key]["folds_by_metrics"][result_key])
                         for model_key in model_keys
                     ],
                     index=model_keys,
@@ -115,7 +120,7 @@ class RepeatedResult:
         """Summary property that returns a DataFrame with aggregated metrics.
 
         Returns:
-          (pd.DataFrame): DataFrame with aggregated metrics.
+            (pd.DataFrame): DataFrame with aggregated metrics.
 
         """
         return self._summary_df
@@ -123,49 +128,50 @@ class RepeatedResult:
 
 class RepeatedCV(CrossValidation):
     """Class for repeated cross-validation. Inherits from CrossValidation.
-    
+
     Methods:
-      set_n_repeats: method that sets the number of repeated runs
-      set_seeds: method that sets the random seeds for the repeated runs
-      set_neptune: method that sets the neptune credentials for the repeated runs and initializes them
-      set_run: in contrast to CrossValidation, the set_run method takes in a parent run and a list of children runs
-      perform: method that performs repeated cross-validation
+        set_n_repeats: method that sets the number of repeated runs
+        set_seeds: method that sets the random seeds for the repeated runs
+        set_neptune: method that sets the neptune credentials for the repeated runs and initializes them
+        set_run: in contrast to CrossValidation, the set_run method takes in a parent run and a list of children runs
+        perform: method that performs repeated cross-validation
 
     Example:
-      ```python
-      from flexcv.synthesizer import generate_regression
-      from flexcv.models import LinearModel
-      from flexcv.model_mapping import ModelConfigDict, ModelMappingDict
+        ```python
+        from flexcv.synthesizer import generate_regression
+        from flexcv.models import LinearModel
+        from flexcv.model_mapping import ModelConfigDict, ModelMappingDict
+        from flexcv.repeated import RepeatedCV
 
-      # make sample data
-      X, y, group, random_slopes = generate_regression(10, 100, n_slopes=1, noise=9.1e-2)
+        # make sample data
+        X, y, group, random_slopes = generate_regression(10, 100, n_slopes=1, noise_level=9.1e-2)
 
-      # create a model mapping
-      model_map = ModelMappingDict(
-          {
-              "LinearModel": ModelConfigDict(
-                  {
-                      "model": LinearModel,
-                      "requires_formula": True,
-                  }
-              ),
-          }
-      )
+        # create a model mapping
+        model_map = ModelMappingDict(
+            {
+                "LinearModel": ModelConfigDict(
+                    {
+                        "model": LinearModel,
+                        "requires_formula": True,
+                    }
+                ),
+            }
+        )
 
-      credentials = {your_neptune_credentials}
+        credentials = {your_neptune_credentials}
 
-      rcv = (
-          RepeatedCV()
-          .set_data(X, y, group, dataset_name="ExampleData")
-          .set_models(model_map)
-          .set_n_repeats(3)
-          .set_neptune(credentials)
-          .perform()
-          .get_results()
-      )
+        rcv = (
+                RepeatedCV()
+                .set_data(X, y, group, dataset_name="ExampleData")
+                .set_models(model_map)
+                .set_n_repeats(3)
+                .set_neptune(credentials)
+                .perform()
+                .get_results()
+        )
 
-      rcv.summary.to_excel("repeated_cv.xlsx")  # save dataframe to excel file
-      ```
+        rcv.summary.to_excel("repeated_cv.xlsx")  # save dataframe to excel file
+        ```
     """
 
     def __init__(self, **kwargs):
@@ -177,10 +183,10 @@ class RepeatedCV(CrossValidation):
         """Set the number of repeats for repeated cross-validation.
 
         Args:
-          n_repeats(int): Number of repeated runs.
+            n_repeats(int): Number of repeated runs.
 
         Returns:
-          (RepeatedCV): self
+            (RepeatedCV): self
 
         """
         self.n_repeats = n_repeats
@@ -191,10 +197,10 @@ class RepeatedCV(CrossValidation):
         If you do not want to pass your credentials explicitly, use set_run and call init_repeated_runs from outside the class yourself.
 
         Args:
-          neptune_credentials (dict): Your API token and project name.
+            neptune_credentials (dict): Your API token and project name.
 
         Returns:
-          (RepeatedCV): self
+            (RepeatedCV): self
 
         """
         self.neptune_credentials = neptune_credentials
@@ -212,11 +218,11 @@ class RepeatedCV(CrossValidation):
         Therefore, the random seed for the repeated runs is set to the same value for each repeated run and is deterministic.
 
         Args:
-          seeds (list[int]): The list of seeds to use in the repeats. (Default value = None)
-          generator_seed (int): Seed to control generation of a list of seeds. (Default value = 42)
+            seeds (list[int]): The list of seeds to use in the repeats. (Default value = None)
+            generator_seed (int): Seed to control generation of a list of seeds. (Default value = 42)
 
         Returns:
-          (RepeatedCV): self
+            (RepeatedCV): self
         """
         np.random.seed(generator_seed)
         self.seeds = np.random.randint(42000, size=self.n_repeats).tolist()
@@ -227,11 +233,11 @@ class RepeatedCV(CrossValidation):
         """Use this method if you want to pass your own parent run and children runs.
 
         Args:
-          parent_run (neptune.run): A run to track the repeated meta data. (Default value = None)
-          children_run list[neptune.run]: A list of runs to track the single runs. (Default value = None)
+            parent_run (neptune.run): A run to track the repeated meta data. (Default value = None)
+            children_run list[neptune.run]: A list of runs to track the single runs. (Default value = None)
 
         Returns:
-          (RepeatedCV): self
+            (RepeatedCV): self
 
         """
         self.parent_run = parent_run
@@ -242,34 +248,56 @@ class RepeatedCV(CrossValidation):
         """Performs repeated cross-validation.
 
         Returns:
-          (pd.DataFrame): DataFrame with aggregated metrics.
+            (pd.DataFrame): DataFrame with aggregated metrics.
 
         """
         add_module_handlers(logger)
-        repeated_run = self.parent_run  # neptune.init_run(**your_credentials)
+        try:
+            repeated_run = self.parent_run  # neptune.init_run(**your_credentials)
+        except AttributeError:
+            logger.info("No parent run found. Initializing dummy run.")
+            repeated_run = Run()
+            self.parent_run = repeated_run
+            self.children_runs = [Run() for _ in range(self.n_repeats)]
+
         repeated_id = repeated_run["sys/id"].fetch()
-        desc = f"Instance of repeated run {repeated_id}."
 
         run_ids = []
         run_results = []
         for seed, inner_run in zip(self.seeds, self.children_runs):
+            # handle neptune
             inner_id = inner_run["sys/id"].fetch()
+            
+            # if inner_id is not a string, we are not using neptune and need to generate a run id
+            if not isinstance(inner_id, str):
+                i = - 1
+                inner_id = f"run_{i+1}"
+            
             inner_run["HostRun"] = repeated_id
             inner_run["seed"] = seed
 
-            results = (
-                self.set_run(run=inner_run, random_seed=seed).perform().get_results()
-            )
+            # instantiate a new CrossValidation instance for each run
+            cv_in = CrossValidation()
+
+            # assign all necessary attributes of self to the inner cv instance
+            for key, value in self.config.items():
+                cv_in.config[key] = value
+
+            # set inner cv config
+            cv_in.config["random_seed"] = seed
+            cv_in.config["run"] = inner_run
+
+            # perform single run and get the results
+            results = cv_in.perform().get_results()
 
             # append the run id and the run metric to the lists
             run_ids.append(inner_id)
             run_results.append(results)
-
+            inner_run.stop()
         # run_dfs have the same column and index names and we
         df = aggregate_(run_results)
 
         # log the repeated run results to neptune
-        repeated_id = repeated_run["sys/id"].fetch()
         repeated_run["summary"].upload(File.as_html(df))
         repeated_run[
             "sys/description"
@@ -284,7 +312,7 @@ class RepeatedCV(CrossValidation):
         """Wrapper method to perform repeated cross-validation.
 
         Returns:
-          (RepeatedCV): self
+            (RepeatedCV): self
 
         """
         if self.seeds is None:
@@ -297,10 +325,10 @@ class RepeatedCV(CrossValidation):
         """Returns the results of repeated cross-validation.
 
         Returns:
-          (RepeatedResult): RepeatedResult object with summary property that returns a DataFrame with aggregated metrics.
+            (RepeatedResult): RepeatedResult object with summary property that returns a DataFrame with aggregated metrics.
         """
         return RepeatedResult(self._summary_df)
 
 
 if __name__ == "__main__":
-  pass
+    pass
