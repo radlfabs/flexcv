b21f5d73f85ebc8c14d45d91aa37e231cd244c0d
radlfabs <rosenthal.fabian@gmail.com>
2023-11-14 22:30:13 +0100

    update doc strings update tests

--- flexcv/yaml_parser.py
+++ flexcv/yaml_parser.py
@@ -5,22 +5,53 @@ from flexcv.model_mapping import ModelConfigDict, ModelMappingDict
 
 
 def int_constructor(loader: yaml.SafeLoader, node: yaml.nodes.ScalarNode) -> IntDistribution:
-    """Construct an IntDistribution."""
+    """Construct an optuna IntDistribution from a yaml node.
+    
+    Args:
+        loader (yaml.SafeLoader): The yaml loader.
+        node (yaml.nodes.ScalarNode): The yaml node.
+    
+    Returns:
+        (IntDistribution): The constructed IntDistribution.
+    """
     return IntDistribution(**loader.construct_mapping(node))
 
 
 def float_constructor(loader: yaml.SafeLoader, node: yaml.nodes.ScalarNode) -> FloatDistribution:
-    """Construct an FloatDistribution."""
+    """Construct an optuna FloatDistribution from a yaml node.
+    
+    Args:
+        loader (yaml.SafeLoader): The yaml loader.
+        node (yaml.nodes.ScalarNode): The yaml node.
+        
+    Returns:
+        (FloatDistribution): The constructed FloatDistribution.
+    """
     return FloatDistribution(**loader.construct_mapping(node))
 
 
 def cat_constructor(loader: yaml.SafeLoader, node: yaml.nodes.ScalarNode) -> CategoricalDistribution:
-    """Construct an FloatDistribution."""
+    """Construct an optuna CategoricalDistribution from a yaml node.
+    
+    Args:
+        loader (yaml.SafeLoader): The yaml loader.
+        node (yaml.nodes.ScalarNode): The yaml node.
+        
+    Returns:
+        (CategoricalDistribution): The constructed CategoricalDistribution.
+    """
     value = loader.construct_mapping(node, deep=True)
     return CategoricalDistribution(**value)
     
     
 def get_loader():
+    """This function returns a yaml loader with the custom constructors for the optuna distributions.
+    Custom and safe constructors are added to the following tags: 
+    
+    - !Int
+    - !Float 
+    - !Cat
+    """
     loader = yaml.SafeLoader
     loader.add_constructor("!Int", int_constructor)
     loader.add_constructor("!Float", float_constructor)
@@ -29,13 +60,30 @@ def get_loader():
 
 
 def split_import(import_string) -> tuple[str, str]:
+    """This function splits an import string into the class name and the module path.
+    
+    Args: 
+        import_string (str): The import string.
+    
+    Returns:
+        (tuple[str, str]): The class name and the module path.    
+    """
     import_list = import_string.split(".")
     class_name = import_list[-1]
     module_path = ".".join(import_list[:-1])
     return class_name, module_path
 
 
-def parse_yaml_output_to_mapping_dict(yaml_dict):
+def parse_yaml_output_to_mapping_dict(yaml_dict) -> ModelMappingDict:
+    """This function parses the output of the yaml parser to a ModelMappingDict object.
+    Models and post processors are imported automatically.
+    
+    Args:
+        yaml_dict (dict): The output of the yaml parser.
+        
+    Returns:
+        (ModelMappingDict): A dictionary of ModelConfigDict objects.
+    """
     model_mapping_dict = ModelMappingDict()
     for model_name, raw_dict in yaml_dict.items():
         if "model" not in raw_dict:
@@ -58,14 +106,68 @@ def parse_yaml_output_to_mapping_dict(yaml_dict):
     return model_mapping_dict
 
 
-def read_mapping_from_yaml_file(yaml_file_path):
+def read_mapping_from_yaml_file(yaml_file_path: str) -> ModelMappingDict:
+    """This function reads in a yaml file and returns a ModelMappingDict object.
+    Use the yaml tags !Int, !Float, !Cat to specify the type of the hyperparameter distributions.
+    The parser takes care of importing the classes specified in the yaml file in the fields model and post_processor.
+    
+    Args:
+        yaml_code (str): The yaml code.
+        
+    Returns:
+        (ModelMappingDict): A dictionary of ModelConfigDict objects.
+    
+    Example:
+        Your file.yaml file could look like this:
+        ```yaml
+        RandomForest:
+            model: sklearn.ensemble.RandomForestRegressor
+            post_processor: flexcv.model_postprocessing.MixedEffectsPostProcessor
+            requires_inner_cv: True
+            params:
+                max_depth: !Int
+                    low: 1
+                    high: 10
+        ```
+        And you would read it in like this:
+        ```python
+        model_mapping = read_mapping_from_yaml_file("file.yaml")
+        ```
+    """
     with open(yaml_file_path, "r") as f:
         yaml_output = yaml.load(f, Loader=get_loader())
     model_mapping = parse_yaml_output_to_mapping_dict(yaml_output)
     return model_mapping
 
 
-def read_mapping_from_yaml_string(yaml_code):
+def read_mapping_from_yaml_string(yaml_code: str) -> ModelMappingDict:
+    """This function reads a yaml string and returns a ModelMappingDict object.
+    Use the yaml tags !Int, !Float, !Cat to specify the type of the hyperparameter distributions.
+    The parser takes care of importing the classes specified in the yaml file in the fields model and post_processor.
+    
+    Args:
+        yaml_code (str): The yaml code.
+        
+    Returns:
+        (ModelMappingDict): A dictionary of ModelConfigDict objects.
+    
+    Example:
+
+        ```python
+        yaml_code = '''
+                    RandomForest:
+                        model: sklearn.ensemble.RandomForestRegressor
+                        post_processor: flexcv.model_postprocessing.MixedEffectsPostProcessor
+                        requires_inner_cv: True
+                        params:
+                            max_depth: !Int
+                                low: 1
+                                high: 10
+                    '''
+        model_mapping = read_mapping_from_yaml_string(yaml_code)
+        ```
+        
+    """
     yaml_output = yaml.load(yaml_code, Loader=get_loader())
     model_mapping = parse_yaml_output_to_mapping_dict(yaml_output)
     return model_mapping
