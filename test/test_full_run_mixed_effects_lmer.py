e1eed29664c96a74c25bd64bf9259434d02d534b
radlfabs <rosenthal.fabian@gmail.com>
2023-11-17 15:24:22 +0100

    test lmer on smaller size

--- test/test_full_run_mixed_effects_lmer.py
+++ test/test_full_run_mixed_effects_lmer.py
@@ -1,16 +1,14 @@
 import numpy as np
-from data import DATA_TUPLE_3_100
+from data import DATA_TUPLE_3_25
 
 from flexcv.interface import CrossValidation
 from flexcv.model_mapping import ModelConfigDict, ModelMappingDict
 from flexcv.model_postprocessing import LMERModelPostProcessor
 from flexcv.models import LinearMixedEffectsModel
-from flexcv.run import Run
-from flexcv.synthesizer import generate_regression
 
 
 def lmer_regression():
-    X, y, group, random_slopes = DATA_TUPLE_3_100
+    X, y, group, random_slopes = DATA_TUPLE_3_25
 
     model_map = ModelMappingDict(
         {
@@ -30,7 +28,6 @@ def lmer_regression():
         .set_splits(n_splits_out=3, n_splits_in=3, break_cross_val=True)
         .set_models(model_map)
         .set_lmer(predict_known_groups_lmm=True)
-        .set_run(run=Run())
         .perform()
         .get_results()
     )
