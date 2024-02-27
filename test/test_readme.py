cc2fd9f6ebc8836db247a80225d7947a5cfb73e6
radlfabs <rosenthal.fabian@gmail.com>
2024-02-27 10:42:11 +0100

    fix bug TypeError, SyntaxError in readme

---
+++ test/test_readme.py
@@ -0,0 +1,15 @@
+import pathlib
+
+import pytest
+from mktestdocs import check_md_file
+from neptune.exceptions import NeptuneInvalidApiTokenException
+
+
+# @pytest.mark.xfail(raises=OSError)
+@pytest.mark.xfail(raises=NeptuneInvalidApiTokenException)
+@pytest.mark.parametrize("fpath", pathlib.Path(".").glob("README.md"), ids=["README"])
+def test_readme_codeblocks_valid(fpath):
+    # Test if the code blocks in the docs files are valid
+    # ignore the following Exception NeptuneInvalidApiTokenException
+    # as this is a valid exception
+    check_md_file(fpath=fpath)
